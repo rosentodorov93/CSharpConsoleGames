6201a231c263e68772e0e91cdac2c7223dbe569f
Rosen Todorov <rosen.vt55@gmail.com>
2023-02-24 18:57:14 +0200

    Added tetris figures drawCurrrent figure method and user input controls

--- Tetris/Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Tetris/Program.cs
@@ -8,10 +8,52 @@
         static int InfoCols = 10;
         static int GameRows = TetrisRows + 2;
         static int GameCols = TetrisCols + InfoCols + 3;
+        static List<bool[,]> TerisFigures = new List<bool[,]>()
+        {
+            new bool[,]
+            {
+                {true, true,true,true}
+            },
+            new bool[,]
+            {
+                {true, true,},
+                {true, true,}
+            },
+            new bool[,]
+            {
+                {false, true,false},
+                {true, true,true}
+            },
+            new bool[,]
+            {
+                {true, true,false},
+                {false, true,true}
+            },
+            new bool[,]
+            {
+                {false, true,true},
+                {true, true,false}
+            },
+            new bool[,]
+            {
+                {true, false,false},
+                {true, true,true}
+            },
+            new bool[,]
+            {
+                {false, false,true},
+                {true, true,true}
+            }
+        };
 
 
         //game state
-
+        static int Frame = 0;
+        static int FramesPerSecond = 15;
+        static int Score = 0;
+        static int CurrentFigureIndex = 2;
+        static int CurrentFigureRow = 0;
+        static int CurrentFigureCol = 0;
 
 
         static void Main(string[] args)
@@ -25,20 +67,77 @@
             DrawBorder();
             while (true)
             {
+                Frame++;
+                //read input
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
 
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        Environment.Exit(0);
+                    }
+                    if (key.Key == ConsoleKey.D || key.Key == ConsoleKey.RightArrow)
+                    {
+                        CurrentFigureCol++;
+                    }
+                    if (key.Key == ConsoleKey.A || key.Key == ConsoleKey.LeftArrow)
+                    {
+                        CurrentFigureCol--;
+                    }
+                    if (key.Key == ConsoleKey.S || key.Key == ConsoleKey.DownArrow)
+                    {
+                        CurrentFigureRow++;
+                    }
+                }
+                //change game state
+                if (Frame % FramesPerSecond == 0)
+                {
+                    Score++;
+                    Frame = 1;
+                    CurrentFigureRow++;
+                }
+                //redraw UI
+                DrawBorder();
+                DrawGameInfo();
+                DrawCurrentFigure();
+                Thread.Sleep(40);
+                
             }
-            //while loop
-            //read input
-            //change game state
-            //redraw UI
-            ///draw border
-            ///draw gameinfo
-            //sleep
+
+
+        }
+
+        private static void DrawCurrentFigure()
+        {
+            var currentFigure = TerisFigures[CurrentFigureIndex];
+
+            for (int r = 0; r < currentFigure.GetLength(0); r++)
+            {
+                for (int c = 0; c < currentFigure.GetLength(1); c++)
+                {
+                    if (currentFigure[r,c])
+                    {
+                        Draw("*", r + CurrentFigureRow + 1, c + CurrentFigureCol + 1, ConsoleColor.Yellow);
+                    }
+                }
+            }
+        }
+
+        private static void DrawGameInfo()
+        {
+            Draw("Frame:", 2, TetrisCols + 2, ConsoleColor.DarkCyan);
+            Draw(Frame.ToString(), 3, TetrisCols + 2, ConsoleColor.DarkCyan);
+
+            Draw("Score:", 4, TetrisCols + 2, ConsoleColor.DarkCyan);
+            Draw(Score.ToString(), 5, TetrisCols + 2, ConsoleColor.DarkCyan);
 
         }
 
         private static void DrawBorder()
         {
+            Console.SetCursorPosition(0,0);
+
             string startLine = "╔";
             startLine += new string('═', TetrisCols);
             startLine += "╦";
