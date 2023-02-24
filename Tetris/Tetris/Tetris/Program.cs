46e78c5dcb0e46ed32194761b975fd42f7922b78
Rosen Todorov <rosen.vt55@gmail.com>
2023-02-24 18:12:57 +0200

    Created tetris game project and added initial setting and main draw method

---
+++ Tetris/Tetris/Tetris/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Tetris
+{
+    internal class Program
+    {
+        //settings
+        static int TetrisRows = 20;
+        static int TetrisCols = 10;
+        static int InfoCols = 10;
+
+
+        //game state
+
+
+
+        static void Main(string[] args)
+        {
+            //while loop
+            //read input
+            //change game state
+            //redraw UI
+            ///draw border
+            ///draw gameinfo
+            
+                  
+        }
+
+        private static void Draw(string symbol, int row, int col, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(col, row);
+            Console.Write(symbol);
+            Console.ResetColor();
+        }
+    }
+}
