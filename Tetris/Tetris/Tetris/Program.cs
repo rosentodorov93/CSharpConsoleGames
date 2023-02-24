c746fe9f46c157bd0328ceccb82ccf36dda801bf
Rosen Todorov <rosen.vt55@gmail.com>
2023-02-24 18:29:19 +0200

    Added drawborder method and set teris size

--- Tetris/Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Tetris/Program.cs
@@ -6,6 +6,8 @@
         static int TetrisRows = 20;
         static int TetrisCols = 10;
         static int InfoCols = 10;
+        static int GameRows = TetrisRows + 2;
+        static int GameCols = TetrisCols + InfoCols + 3;
 
 
         //game state
@@ -14,14 +16,52 @@
 
         static void Main(string[] args)
         {
+            Console.Title = "Tetris-v1.0";
+            Console.WindowHeight = GameRows + 1;
+            Console.WindowWidth = GameCols + 1;
+            Console.BufferHeight = GameRows + 1;
+            Console.BufferWidth = GameCols + 1;
+            Console.CursorVisible = false;
+            DrawBorder();
+            while (true)
+            {
+
+            }
             //while loop
             //read input
             //change game state
             //redraw UI
             ///draw border
             ///draw gameinfo
-            
-                  
+            //sleep
+
+        }
+
+        private static void DrawBorder()
+        {
+            string startLine = "╔";
+            startLine += new string('═', TetrisCols);
+            startLine += "╦";
+            startLine += new string('═', InfoCols);
+            startLine += "╗";
+            Console.WriteLine(startLine);
+
+            for (int i = 0; i < TetrisRows; i++)
+            {
+                string line = "║";
+                line += new string(' ', TetrisCols);
+                line += "║";
+                line += new string(' ', InfoCols);
+                line += "║";
+                Console.WriteLine(line);
+            }
+
+            string endLine = "╚";
+            endLine += new string('═', TetrisCols);
+            endLine += "╩";
+            endLine += new string('═', InfoCols);
+            endLine += "╝";
+            Console.WriteLine(endLine);
         }
 
         private static void Draw(string symbol, int row, int col, ConsoleColor color = ConsoleColor.White)
