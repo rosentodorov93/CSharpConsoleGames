43af1c5a79e85936fd333ff3c9a0f3a13a7f893a
Rosen Todorov <rosen.vt55@gmail.com>
2023-03-28 19:36:18 +0300

    Added basic Index class for board position

---
+++ TicTacToe/TicTacToe/TicTacToe/Index.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Index
+    {
+        public Index(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Row}-{this.Col}";
+        }
+    }
+}
