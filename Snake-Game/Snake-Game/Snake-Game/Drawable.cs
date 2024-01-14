3793fd5cf6f9b62d0917d1909ea0df009378e82d
Rosen Todorov <rosen.vt55@gmail.com>
2024-01-14 14:04:17 +0200

    Added helping classes for drawing and position

---
+++ Snake-Game/Snake-Game/Snake-Game/Drawable.cs
@@ -0,0 +1,12 @@
+﻿namespace Snake_Game
+{
+    abstract class Drawable
+    {
+        public void Draw(Position position, char symbol, ConsoleColor color = ConsoleColor.Yellow)
+        {
+            Console.SetCursorPosition(position.row, position.col);
+            Console.ForegroundColor = color;
+            Console.Write(symbol);
+        }
+    }
+}
