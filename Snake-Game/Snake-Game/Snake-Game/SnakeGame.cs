9d362ff9538cdf4f48d70d547ee8480223a8b070
Rosen Todorov <rosen.vt55@gmail.com>
2024-01-14 19:07:12 +0200

    Added snakeGame class with main loop

---
+++ Snake-Game/Snake-Game/Snake-Game/SnakeGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    public class SnakeGame
+    {
+        private Snake snake;
+
+        public SnakeGame(Snake snake)
+        {
+            this.snake = snake;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                snake.DrawSnake();
+            }
+        }
+    }
+}
